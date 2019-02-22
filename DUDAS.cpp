056ff8896a9d3b53a0916a8611162dc0c7d6f139
Nicolas <nicolasmiranda@javeriana.edu.co>
2019-02-22 10:53:16 -0500

    nuevo archivo creado

---
+++ DUDAS.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //CONTADORES
+    /*
+    int contador = 0;
+    cout << contador << endl;   //0
+    contador = contador + 1;
+    cout << contador << endl;   //1
+    contador += 1;
+    cout << contador << endl;   //2
+    contador++;
+    cout << contador << endl;   //3
+    cout << contador + 1 << endl;   //IMPRIME 4 - CONTADOR VALE 3
+    cout << contador << endl;   //3
+    */
+    //ACUMULADORES
+    /*
+    int acumulador, dato;
+    char respuesta = 'S';
+    acumulador = 5;
+    //RESPUESTA DE USUARIO
+    while(respuesta == 'S' || respuesta == 's'){
+        cout << "ACUM: " << acumulador << endl;
+        cout << "Cuanto desea aumentar el acumulador? ";
+        cin >> dato;
+        //acumulador = acumulador + dato;
+        acumulador += dato;
+        cout << "Desea continuar? (S/N): ";
+        cin >> respuesta;
+    }
+    */
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
